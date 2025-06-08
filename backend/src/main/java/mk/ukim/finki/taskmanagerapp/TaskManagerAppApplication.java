15b8d9e444f1978098225810dda2fbf0574917f2
dimitarbalo <dimitarbalo@gmail.com>
2025-06-08 14:07:43 +0200

    Remove dotenv usage from main class

--- backend/src/main/java/mk/ukim/finki/taskmanagerapp/TaskManagerAppApplication.java
+++ backend/src/main/java/mk/ukim/finki/taskmanagerapp/TaskManagerAppApplication.java
@@ -9,9 +9,6 @@ public class TaskManagerAppApplication {
 
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(TaskManagerAppApplication.class, args);
     }
